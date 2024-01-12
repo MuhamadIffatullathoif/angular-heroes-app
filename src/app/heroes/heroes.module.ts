63faec5f21ba03dae6265370a5b9aa160c6647e8
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-12 09:11:01 +0700

    10. Show List of Heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroPageComponent } from './pages/hero-page/hero-page.component';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
-import { NewPageComponent } from './pages/new-page/new-page.component';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
+import {HeroesRoutingModule} from './heroes-routing.module';
+import {HeroPageComponent} from './pages/hero-page/hero-page.component';
+import {LayoutPageComponent} from './pages/layout-page/layout-page.component';
+import {ListPageComponent} from './pages/list-page/list-page.component';
+import {NewPageComponent} from './pages/new-page/new-page.component';
+import {SearchPageComponent} from './pages/search-page/search-page.component';
 import {MaterialModule} from "../material/material.module";
+import {HeroCardComponent} from './components/hero-card/hero-card.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import {MaterialModule} from "../material/material.module";
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    HeroCardComponent
   ],
   imports: [
     CommonModule,
@@ -24,4 +26,5 @@ import {MaterialModule} from "../material/material.module";
     MaterialModule
   ]
 })
-export class HeroesModule { }
+export class HeroesModule {
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,11 +6,17 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatListModule} from "@angular/material/list";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatCardModule} from "@angular/material/card";
+import {MatChipsModule} from "@angular/material/chips";
 
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
