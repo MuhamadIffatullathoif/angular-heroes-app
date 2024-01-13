d6d6d139bc701acd4c94f29238296fd649ee114f
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-13 15:59:16 +0700

    25. Auth Service - Authentication Service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environments} from "../../../environments/environments";
+import {User} from "../interfaces/user.interface";
+import {Observable, tap} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl: string = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) {
+  }
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap((user: User) => this.user = user),
+        tap((user: User) => localStorage.setItem("token",user.id.toString()))
+      );
+  }
+}
