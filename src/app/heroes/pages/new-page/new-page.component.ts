bfc4bcd651ad11a9957fdfaf9a9dd782e24dc7b8
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-13 15:00:38 +0700

    23. Material Dialog

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -5,6 +5,8 @@ import {HeroesService} from "../../services/heroes.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {switchMap} from "rxjs";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog} from "@angular/material/dialog";
+import {ConfirmDialogComponent} from "../../components/confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: 'app-new-page',
@@ -29,7 +31,8 @@ export class NewPageComponent implements OnInit {
   constructor(private heroesService: HeroesService,
               private activatedRoute: ActivatedRoute,
               private router: Router,
-              private snackBar: MatSnackBar) {
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -38,10 +41,10 @@ export class NewPageComponent implements OnInit {
       .pipe(
         switchMap(({id}) => this.heroesService.getHeroById(id))
       ).subscribe(hero => {
-        if(!hero) return this.router.navigateByUrl('/');
+      if (!hero) return this.router.navigateByUrl('/');
 
-        this.heroForm.reset(hero);
-        return;
+      this.heroForm.reset(hero);
+      return;
     })
   }
 
@@ -73,6 +76,19 @@ export class NewPageComponent implements OnInit {
   showSnackbar(message: string): void {
     this.snackBar.open(message, 'done', {
       duration: 2500
+    })
+  }
+
+  onDeleteHero(): void {
+    if (!this.currentHero.id) throw new Error('Hero id is required');
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (!result) return;
+      this.heroesService.deleteHeroById(this.currentHero.id).subscribe();
+      this.router.navigate(['/heroes']);
     })
   }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -15,6 +15,7 @@ import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatOptionModule} from "@angular/material/core";
 import {MatSelectModule} from "@angular/material/select";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatDialogModule} from "@angular/material/dialog";
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
+    MatDialogModule,
     MatDividerModule,
     MatFormFieldModule,
     MatGridListModule,
